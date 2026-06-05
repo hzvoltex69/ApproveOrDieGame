b7ac95a6257ee87d9a9041258b635633fbb042b9
leoliu69 <patricio.quispe.au@mail.pucv.cl>
2026-06-05 00:54:39 -0400

    add MenuBase abstract class

---
+++ core/src/com/mygdx/game/screens/MenuBase.java
@@ -0,0 +1,68 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.ApproveOrDieGame;
+
+public abstract class MenuBase implements Screen {
+	protected final ApproveOrDieGame game;
+	protected SpriteBatch batch;	   
+	protected BitmapFont font;
+	protected OrthographicCamera camera;
+	
+	public MenuBase(ApproveOrDieGame game) {
+		this.game = game;
+        this.batch = game.getBatch();
+        this.font = game.getFont();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+	}
+	
+	@Override
+	public final void render(float delta) {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		renderContent();
+		batch.end();
+		handleInput();
+	}
+
+	protected abstract void renderContent();
+	protected abstract void handleInput();
+	
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
