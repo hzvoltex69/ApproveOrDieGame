cf3ac133f3c3c9ae6133366582c046dc0881ab20
Fer GD <fernando.guerrero.d01@mail.pucv.cl>
2026-06-05 18:05:12 -0400

    Se agrega la clase GoodDrop

---
+++ core/src/com/mygdx/game/drops/GoodDrop.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.drops;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.audio.AudioManager;
+import com.mygdx.game.entities.Student;
+
+public class GoodDrop extends Drop {
+
+	public GoodDrop(Texture texture, float speed) {
+		super(texture, speed);
+	}
+
+	@Override
+	protected void onCollect(Student student) {
+		collected = true;
+		student.sumarPuntos(10);
+		AudioManager.getInstance().playDrop();
+	}
+
+	@Override
+	protected void animate(SpriteBatch batch) {
+		batch.draw(texture, bounds.x, bounds.y);
+	}
+}
