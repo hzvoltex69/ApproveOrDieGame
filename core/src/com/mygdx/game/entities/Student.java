0d125e658660957b29f869dddd97cc685c086104
Fer GD <fernando.guerrero.d01@mail.pucv.cl>
2026-06-05 13:23:24 -0400

    Actualizar la logica interna de la clase Student

--- core/src/com/mygdx/game/entities/Student.java
+++ core/src/com/mygdx/game/entities/Student.java
@@ -1,94 +1,81 @@
-package puppy.code;
+package com.mygdx.game.entities;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.game.audio.AudioManager;
+
+public class Student implements Drawable {
+
+	private Rectangle student;
+	private Texture studentImage;
+	private int vidas = 3;
+	private int puntos = 0;
+	private int velx = 400;
+	private boolean herido = false;
+	private int tiempoHeridoMax = 50;
+	private int tiempoHerido;
+
+	public Student(Texture tex) {
+		studentImage = tex;
+	}
+
+	public void crear() {
+		student = new Rectangle();
+		student.x = 800 / 2 - 64 / 2;
+		student.y = 20;
+		student.width = 64;
+		student.height = 64;
+	}
+
+	public void danar() {
+		vidas--;
+		herido = true;
+		tiempoHerido = tiempoHeridoMax;
+		AudioManager.getInstance().playHurt();
+	}
+
+	public void addLife() {
+		if (vidas < 3) vidas++;
+	}
 
 
-public class Tarro {
-	   private Rectangle bucket;
-	   private Texture bucketImage;
-	   private Sound sonidoHerido;
-	   private int vidas = 3;
-	   private int puntos = 0;
-	   private int velx = 400;
-	   private boolean herido = false;
-	   private int tiempoHeridoMax=50;
-	   private int tiempoHerido;
-	   
-	   
-	   public Tarro(Texture tex, Sound ss) {
-		   bucketImage = tex;
-		   sonidoHerido = ss;
-	   }
-	   
-		public int getVidas() {
-			return vidas;
+	public void draw(SpriteBatch batch) {
+		if (!herido)
+			batch.draw(studentImage, student.x, student.y);
+		else {
+			batch.draw(studentImage, student.x, student.y + MathUtils.random(-5, 5));
+			tiempoHerido--;
+			if (tiempoHerido <= 0) herido = false;
 		}
-	
-		public int getPuntos() {
-			return puntos;
-		}
-		public Rectangle getArea() {
-			return bucket;
-		}
-		public void sumarPuntos(int pp) {
-			puntos+=pp;
-		}
-		
-	
-	   public void crear() {
-		      bucket = new Rectangle();
-		      bucket.x = 800 / 2 - 64 / 2;
-		      bucket.y = 20;
-		      bucket.width = 64;
-		      bucket.height = 64;
-	   }
-	   public void dañar() {
-		  vidas--;
-		  herido = true;
-		  tiempoHerido=tiempoHeridoMax;
-		  sonidoHerido.play();
-	   }
-	   public void dibujar(SpriteBatch batch) {
-		 if (!herido)  
-		   batch.draw(bucketImage, bucket.x, bucket.y);
-		 else {
-		
-		   batch.draw(bucketImage, bucket.x, bucket.y+ MathUtils.random(-5,5));
-		   tiempoHerido--;
-		   if (tiempoHerido<=0) herido = false;
-		 }
-	   } 
-	   
-	   
-	   public void actualizarMovimiento() { 
-		   // movimiento desde mouse/touch
-		   /*if(Gdx.input.isTouched()) {
-			      Vector3 touchPos = new Vector3();
-			      touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-			      camera.unproject(touchPos);
-			      bucket.x = touchPos.x - 64 / 2;
-			}*/
-		   //movimiento desde teclado
-		   if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= velx * Gdx.graphics.getDeltaTime();
-		   if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += velx * Gdx.graphics.getDeltaTime();
-		   // que no se salga de los bordes izq y der
-		   if(bucket.x < 0) bucket.x = 0;
-		   if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-	   }
-	    
+	}
 
-	public void destruir() {
-		    bucketImage.dispose();
-	   }
-	
-   public boolean estaHerido() {
-	   return herido;
-   }
-	   
+	public void actualizarMovimiento() {
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) student.x -= velx * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) student.x += velx * Gdx.graphics.getDeltaTime();
+		if (student.x < 0) student.x = 0;
+		if (student.x > 800 - 64) student.x = 800 - 64;
+	}
+
+	public void sumarPuntos(int pp) { 
+		puntos += pp; 
+		}
+	public boolean estaHerido() { 
+		return herido; 
+		}
+	public int getVidas() { 
+		return vidas; 
+		}
+	public int getPuntos() { 
+		return puntos; 
+		}
+	public Rectangle getArea() { 
+		return student; 
+		}
+	public void destruir() { 
+		studentImage.dispose(); 
+		}
 }
