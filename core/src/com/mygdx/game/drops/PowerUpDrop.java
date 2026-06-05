c7086fad6f3f3ce2a87069e7abcc7559cc93a222
Fer GD <fernando.guerrero.d01@mail.pucv.cl>
2026-06-05 19:25:15 -0400

    Se agrega la clase PowerDrop

---
+++ core/src/com/mygdx/game/drops/PowerUpDrop.java
@@ -0,0 +1,24 @@
+package com.mygdx.game.drops;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.audio.AudioManager;
+import com.mygdx.game.entities.Student;
+
+public class PowerUpDrop extends Drop {
+
+	public PowerUpDrop(Texture texture, float speed) {
+		super(texture, speed);
+	}
+
+	protected void onCollect(Student student) {
+	    collected = true;
+	    student.addLife();
+	    AudioManager.getInstance().playHeart();
+	}
+
+	@Override
+	protected void animate(SpriteBatch batch) {
+		batch.draw(texture, bounds.x, bounds.y);
+	}
+}
