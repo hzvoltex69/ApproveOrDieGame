61b6e28e6d34ed9d333ad3f340d6b56b64046067
leoliu69 <patricio.quispe.au@mail.pucv.cl>
2026-06-06 18:39:25 -0400

    add WinScreen

---
+++ core/src/com/mygdx/game/screens/WinScreen.java
@@ -0,0 +1,34 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.mygdx.game.ApproveOrDieGame;
+import com.mygdx.game.GameScreen;
+import com.mygdx.game.audio.AudioManager;
+
+public class WinScreen extends MenuBase {
+
+	public WinScreen(final ApproveOrDieGame game) {
+		super(game);
+	}
+
+	@Override
+	public void show() {
+		AudioManager.getInstance().playLevelUp();
+	}
+
+	@Override
+	protected void renderContent() {
+		font.draw(batch, "Ganaste!!!", 100, 300);
+		font.draw(batch, "Felicidades por aprobar todos tus ramos!", 100, 250);
+		font.draw(batch, "Highest Course: Course " + game.getHighestCourse(), 100, 200);
+		font.draw(batch, "Toca en cualquier lado para reiniciar.", 100, 100);
+	}
+
+	@Override
+	protected void handleInput() {
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
+	}
+}
