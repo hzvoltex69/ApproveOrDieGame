cf2c492757ee84fce54f12bb7022f5f770e00a2d
Fer GD <fernando.guerrero.d01@mail.pucv.cl>
2026-06-06 15:36:26 -0400

    Actualizar la logica interna de la clase GradeRain

--- core/src/com/mygdx/game/entities/GradeRain.java
+++ core/src/com/mygdx/game/entities/GradeRain.java
@@ -1,106 +1,87 @@
-package puppy.code;
+package com.mygdx.game.entities;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.mygdx.game.audio.AudioManager;
+import com.mygdx.game.courses.DropStrategy;
+import com.mygdx.game.drops.BadDrop;
+import com.mygdx.game.drops.Drop;
+import com.mygdx.game.drops.GoodDrop;
+import com.mygdx.game.drops.PowerUpDrop;
 
-public class Lluvia {
-	private Array<Rectangle> rainDropsPos;
-	private Array<Integer> rainDropsType;
-    private long lastDropTime;
-    private Texture gotaBuena;
-    private Texture gotaMala;
-    private Sound dropSound;
-    private Music rainMusic;
-	   
-	public Lluvia(Texture gotaBuena, Texture gotaMala, Sound ss, Music mm) {
-		rainMusic = mm;
-		dropSound = ss;
-		this.gotaBuena = gotaBuena;
-		this.gotaMala = gotaMala;
+public class GradeRain implements Drawable {
+
+	private Array<Drop> drops;
+	private long lastDropTime;
+	private Texture goodTexture;
+	private Texture badTexture;
+	private Texture powerUpTexture;
+	private DropStrategy strategy;
+
+	public GradeRain(Texture goodTexture, Texture badTexture, Texture powerUpTexture) {
+		this.goodTexture = goodTexture;
+		this.badTexture = badTexture;
+		this.powerUpTexture = powerUpTexture;
 	}
-	
+
+	public void setStrategy(DropStrategy strategy) {
+		this.strategy = strategy;
+	}
+
 	public void crear() {
-		rainDropsPos = new Array<Rectangle>();
-		rainDropsType = new Array<Integer>();
-		crearGotaDeLluvia();
-	      // start the playback of the background music immediately
-	      rainMusic.setLooping(true);
-	      rainMusic.play();
+		drops = new Array<Drop>();
+		crearGrades(3);
+		AudioManager.getInstance().playGameSong();
 	}
-	
-	private void crearGotaDeLluvia() {
-	      Rectangle raindrop = new Rectangle();
-	      raindrop.x = MathUtils.random(0, 800-64);
-	      raindrop.y = 480;
-	      raindrop.width = 64;
-	      raindrop.height = 64;
-	      rainDropsPos.add(raindrop);
-	      // ver el tipo de gota
-	      if (MathUtils.random(1,10)<5)	    	  
-	         rainDropsType.add(1);
-	      else 
-	    	 rainDropsType.add(2);
-	      lastDropTime = TimeUtils.nanoTime();
-	   }
-	
-   public boolean actualizarMovimiento(Tarro tarro) { 
-	   // generar gotas de lluvia 
-	   if(TimeUtils.nanoTime() - lastDropTime > 100000000) crearGotaDeLluvia();
-	  
-	   
-	   // revisar si las gotas cayeron al suelo o chocaron con el tarro
-	   for (int i=0; i < rainDropsPos.size; i++ ) {
-		  Rectangle raindrop = rainDropsPos.get(i);
-	      raindrop.y -= 300 * Gdx.graphics.getDeltaTime();
-	      //cae al suelo y se elimina
-	      if(raindrop.y + 64 < 0) {
-	    	  rainDropsPos.removeIndex(i); 
-	    	  rainDropsType.removeIndex(i);
-	      }
-	      if(raindrop.overlaps(tarro.getArea())) { //la gota choca con el tarro
-	    	if(rainDropsType.get(i)==1) { // gota dañina
-	    	  tarro.dañar();
-	    	  if (tarro.getVidas()<=0)
-	    		 return false; // si se queda sin vidas retorna falso /game over
-	    	  rainDropsPos.removeIndex(i);
-	          rainDropsType.removeIndex(i);
-	      	}else { // gota a recolectar
-	    	  tarro.sumarPuntos(10);
-	          dropSound.play();
-	          rainDropsPos.removeIndex(i);
-	          rainDropsType.removeIndex(i);
-	      	}
-	      }
-	   } 
-	  return true; 
-   }
-   
-   public void actualizarDibujoLluvia(SpriteBatch batch) { 
-	   
-	  for (int i=0; i < rainDropsPos.size; i++ ) {
-		  Rectangle raindrop = rainDropsPos.get(i);
-		  if(rainDropsType.get(i)==1) // gota dañina
-	         batch.draw(gotaMala, raindrop.x, raindrop.y); 
-		  else
-			 batch.draw(gotaBuena, raindrop.x, raindrop.y); 
-	   }
-   }
-   public void destruir() {
-      dropSound.dispose();
-      rainMusic.dispose();
-   }
-   public void pausar() {
-	  rainMusic.stop();
-   }
-   public void continuar() {
-	  rainMusic.play();
-   }
-   
+
+	private void crearGrades(int vidas) {
+		float rand = MathUtils.random();
+		Drop drop;
+		if (rand < strategy.getBadDropChance())
+			drop = new BadDrop(badTexture, strategy.getDropSpeed());
+		else if (vidas < 3 && rand < strategy.getBadDropChance() + strategy.getPowerUpChance())
+			drop = new PowerUpDrop(powerUpTexture, strategy.getDropSpeed());
+		else
+			drop = new GoodDrop(goodTexture, strategy.getDropSpeed());
+
+		drop.getBounds().x = MathUtils.random(0, 800 - 64);
+		drop.getBounds().y = 480;
+		drops.add(drop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	public boolean actualizarMovimiento(Student student) {
+		if (TimeUtils.nanoTime() - lastDropTime > (long)(strategy.getSpawnRate() * 1000000000f))
+			crearGrades(student.getVidas());
+
+		for (int i = 0; i < drops.size; i++) {
+			Drop drop = drops.get(i);
+			drop.update(student);
+			if (drop.isOffScreen() || drop.isCollected()) {
+				drops.removeIndex(i);
+				i--;
+			}
+			if (student.getVidas() <= 0)
+				return false;
+		}
+		return true;
+	}
+
+	public void draw(SpriteBatch batch) {
+		for (Drop drop : drops)
+			drop.draw(batch);
+	}
+
+	public void destruir() {
+		AudioManager.getInstance().stopGameSong();
+		goodTexture.dispose();
+		badTexture.dispose();
+		powerUpTexture.dispose();
+	}
+
+	public void pausar() { AudioManager.getInstance().stopGameSong(); }
+	public void continuar() { AudioManager.getInstance().playGameSong(); }
 }
