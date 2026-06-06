d5f73f47ee8ab5f3415ba4fbbcdd1aa7d4c01cec
leoliu69 <patricio.quispe.au@mail.pucv.cl>
2026-06-06 18:50:18 -0400

    add GameOverScreen

---
+++ core/src/com/mygdx/game/screens/GameOverScreen.java
@@ -0,0 +1,36 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.mygdx.game.ApproveOrDieGame;
+import com.mygdx.game.GameScreen;
+import com.mygdx.game.audio.AudioManager;
+
+public class GameOverScreen extends MenuBase {
+
+	private String courseName;
+
+	public GameOverScreen(final ApproveOrDieGame game, String courseName) {
+		super(game);
+		this.courseName = courseName;
+	}
+
+	@Override
+	public void show() {
+		AudioManager.getInstance().playGameOver();
+	}
+
+	@Override
+	protected void renderContent() {
+		font.draw(batch, "REPROBASTE " + courseName + ", you are died JAJAJAJA", 100, 200);
+		font.draw(batch, "Toca en cualquier lado para reiniciar.", 100, 100);
+		font.draw(batch, "Highest Course: Course " + game.getHighestCourse(), 100, 300);
+	}
+
+	@Override
+	protected void handleInput() {
+		if (Gdx.input.isTouched()) {
+			game.setScreen(new GameScreen(game));
+			dispose();
+		}
+	}
+}
