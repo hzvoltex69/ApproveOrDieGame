55ce3c3ee979a4bcd2e41a64e1de35c73fbf8484
leoliu69 <patricio.quispe.au@mail.pucv.cl>
2026-06-07 19:54:50 -0400

    update DesktopLauncher to use ApproveOrDieGame

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -2,14 +2,14 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.GameLluvia;
+import com.mygdx.game.ApproveOrDieGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("My GDX Game");
-		new Lwjgl3Application(new GameLluvia(), config);
+		config.setTitle("ApproveOrDieGame");
+		new Lwjgl3Application(new ApproveOrDieGame(), config);
 	}
 }
