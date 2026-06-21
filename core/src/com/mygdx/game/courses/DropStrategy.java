269fa1b79fef0c293254fc0d2cc51e3d02c53276
leoliu69 <patricio.quispe.au@mail.pucv.cl>
2026-06-20 20:59:11 -0400

    refactor DropStrategy to abstract class with createDrop()

--- core/src/com/mygdx/game/courses/DropStrategy.java
+++ core/src/com/mygdx/game/courses/DropStrategy.java
@@ -1,8 +1,25 @@
 package com.mygdx.game.courses;
 
-public interface DropStrategy {
-	float getDropSpeed();
-	float getSpawnRate();
-	float getBadDropChance();
-	float getPowerUpChance();
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.mygdx.game.drops.BadDrop;
+import com.mygdx.game.drops.Drop;
+import com.mygdx.game.drops.GoodDrop;
+import com.mygdx.game.drops.PowerUpDrop;
+
+public abstract class DropStrategy {
+	public abstract float getDropSpeed();
+	public abstract float getSpawnRate();
+	public abstract float getBadDropChance();
+	public abstract float getPowerUpChance();
+	
+	public Drop createDrop(Texture goodTexture, Texture badTexture, Texture powerUpTexture, int vidas) {
+        float rand = MathUtils.random();
+        if (rand < getBadDropChance())
+            return new BadDrop(badTexture, getDropSpeed());
+        else if (vidas < 3 && rand < getBadDropChance() + getPowerUpChance())
+            return new PowerUpDrop(powerUpTexture, getDropSpeed());
+        else
+            return new GoodDrop(goodTexture, getDropSpeed());
+    }
 }
